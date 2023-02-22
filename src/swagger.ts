8134a6669966b0ab631de632d6ce8bc4b1a4bb72
Blossom <saphashb@gmail.com>
2023-02-22 23:06:58 +0100

    swagger

---
+++ src/swagger.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import {version} from '../../package.json'
+
+const j = version
