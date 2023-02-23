15613af16b39c6c6de8a5583546b151d75e7a59a
Blossom <saphashb@gmail.com>
2023-02-23 22:31:46 +0100

    remove versioning from swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import swaggerJsdoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
-import {version} from '../../package.json'
 
-const j = version
+//const j = version
